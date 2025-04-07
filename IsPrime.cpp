1fe0758adf968b05054b3912aeb34863f9c35e6d
Sharmavicky <sharmavicky93sh@gmail.com>
2025-04-07 11:41:15 +0530

    Check for Prime_Number

--- IsPrime.cpp
+++ IsPrime.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+
+/*
+        Time Complexity (Brute Force Approach): O(n)
+        Time Complexity (Optimal Approach): O(sqrt(n))
+
+        Space Complexity (Brute Force Approach): O(1)
+        Space Complexity (Optimal Approach): O(1)
+*/
 
 class isPrime {
     public:
     bool BruteForce_Approach(int n) {
-        for(int i=2; i<n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
+        int count = 0;
+        for(int i=1; i<=n; i++) {
+            if (n % i == 0) count++;
+        };
+
+        if (count == 2) {
+            return true;
+        } else {
+            return false;
         }
-        return true;
+    };
+
+    bool Optimal_Approach(int n) {
+        int count = 0;
+        for(int i=1; i<=sqrt(n); i++) {
+            if (n % i == 0) {
+                count++;
+                if (n/i != i) count++;
+            }
+        };
+
+        if (count == 2) {
+            return true;
+        } else {
+            return false;
+        }
     };
 };
 
 // Main Function
 int main() {
-    bool result = isPrime().BruteForce_Approach(175);
-    cout << result;
+    bool r1 = isPrime().BruteForce_Approach(175);
+    bool r2 = isPrime().Optimal_Approach(1000000084);
+    cout << r1 << endl;
+    cout << r2 << endl;
     return 0;
 }
